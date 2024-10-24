d923e2e360d099b5db77c304b73eb1f86dfcfa0e
Pablopoke4 <pablomantovani05@gmail.com>
2024-10-23 22:06:48 -0300

    Commit aula do dia 23/10/24

---
+++ DesingPattern/Factory/LiuKang.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class LiuKang : PersonagemBase
+    {
+        public override void Escolhido()
+        {
+            
+            Console.WriteLine(GetName());
+        }
+
+        public override void Atacar()
+        {
+            Console.WriteLine("Ataque" + GetName());
+        }
+
+      
+    }
+}
